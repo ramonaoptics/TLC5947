d832692288309c78bd201c81a9f712d13123c017
Mark Harfouche <mark.harfouche@gmail.com>
2020-07-05 19:51:38 -0400

    latch index

--- TLC5947.cpp
+++ TLC5947.cpp
@@ -251,7 +251,7 @@ void TLC5947::updateLeds_1D(){
   } //end of latch loop
 
   SPI.endTransaction();
-  for (latch_index=0; latch_index < num_latches; latch++){
+  for (int latch_index=0; latch_index < num_latches; latch_index++){
     latch(latch_index);
   }
 
