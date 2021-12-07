3a878941a06e3d8a9a170bfd74fa874da74225c0
Maxwell Zheng <maxwell@ramonaoptics.com>
2021-12-07 18:19:48 -0500

    fix function signature

--- TLC5947.h
+++ TLC5947.h
@@ -68,7 +68,7 @@ void setRgbPinOrder(uint8_t rPos, uint8_t grPos, uint8_t bPos);
 void setPinOrderSingle(uint16_t channel, uint8_t color_channel_index, uint8_t position);
 void setRgbPinOrderSingle(uint16_t channel, uint8_t rPos, uint8_t grPos, uint8_t bPos);
 
-int updateLeds();
+int updateLeds(double* output_current);
 void clearLeds();
 void latch(int latch_index);
 void setSpiBaudRate(uint32_t new_baud_rate);
